bee6d521c523dbb78bec226ac7a062de7bf72682
python-coder223 <masdubek21@gmail.com>
2026-05-12 10:21:47 +0500

    Update app.py

--- app.py
+++ app.py
@@ -159,4 +159,7 @@ def serve_image(filename):
     return send_from_directory(app.config["UPLOAD_FOLDER_IMAGES"], filename)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
